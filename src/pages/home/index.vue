1759ed32acd3a61ec4ea0c4481b0f64e3fc2f048
SEVENTEEN <739829753@qq.com>
2021-09-10 21:06:50 +0800

    feat:登录页面

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,20 +1,8 @@
 <template>
   <p>home</p>
-  <h1>{{ num }}</h1>
-  <el-button type="primary" @click="toLogin">Login</el-button>
 </template>
-<script setup>
-import { computed } from 'vue'
-import { useRouter } from 'vue-router'
-import { useStore } from 'vuex'
+<script>
+import { defineComponent } from 'vue'
 
-const router = useRouter()
-const store = useStore()
-const toLogin = () => {
-  router.push({
-    name: 'login'
-  })
-}
-
-const num = computed(() => store.state.count)
+export default defineComponent({})
 </script>
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,20 +1,93 @@
 <template>
-  <el-button type="primary">{{ 'login' }}</el-button>
-  <p style="margin: 10px">{{ 'torres' }}</p>
-  <el-button type="primary" @click="toHome">{{ 'home' }}</el-button>
+  <div class="login-container">
+    <div class="login-wrapper">
+      <el-form
+        :label-position="labelPosition"
+        label-width="80px"
+        :model="formLabelAlign"
+        :rules="rules"
+        ref="loginForm"
+        class="login_From"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="formLabelAlign.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input show-password v-model="formLabelAlign.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            type="primary"
+            @click="submitForm('loginForm')"
+            class="btns"
+          >
+            登录
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 <script setup>
+import { getCurrentInstance, reactive, ref } from 'vue'
 import { useRouter } from 'vue-router'
+
+const { ctx } = getCurrentInstance()
+
 const router = useRouter()
-const toHome = () => {
-  router.push({
-    name: 'home'
+const labelPosition = ref('right')
+const loginForm = ref(null)
+const formLabelAlign = reactive({
+  username: '',
+  password: ''
+})
+
+const rules = reactive({
+  username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    {
+      min: 6,
+      max: 18,
+      message: '长度在 6 到 18 个字符',
+      trigger: 'blur'
+    }
+  ]
+})
+
+const submitForm = (formName) => {
+  ctx.$refs[formName].validate((valid) => {
+    if (valid) {
+      router.push('/home')
+    } else {
+      console.log('error submit!!')
+      return false
+    }
   })
 }
 </script>
 
 <style scoped lang="less">
-p {
-  color: @color;
+.login-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-wrapper {
+    width: 300px;
+    height: 200px;
+    border-radius: 10px;
+    padding: 20px;
+    background-color: #fff;
+    margin-bottom: 200px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+  }
+  .login_From {
+    margin: 13px 10px 0px -19px;
+  }
+  .btns {
+    margin: 13px 0 0 35px;
+  }
 }
 </style>
