3f99b734ba5c11dbcf32a250e5dc956ffdc4f1fd
SEVENTEEN <739829753@qq.com>
2021-09-19 16:52:44 +0800

    feat: 注册页面

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,30 +1,73 @@
 <template>
   <div class="login-container">
     <div class="login-wrapper">
-      <el-form
-        :label-position="labelPosition"
-        label-width="80px"
-        :model="loginParams"
-        :rules="loginRules"
-        ref="loginForm"
-        class="login_From"
-      >
-        <el-form-item label="用户名" prop="username">
-          <el-input v-model="loginParams.username"></el-input>
-        </el-form-item>
-        <el-form-item label="密码" prop="password">
-          <el-input show-password v-model="loginParams.password"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button
-            type="primary"
-            @click="submitLoginForm('loginForm')"
-            class="btns"
+      <el-tabs v-model="activeName" type="border-card">
+        <!-- 登录页面 -->
+
+        <el-tab-pane label="登录" name="first" class="page">
+          <el-form
+            :label-position="labelPosition"
+            label-width="80px"
+            :model="loginParams"
+            :rules="loginRules"
+            ref="loginForm"
+            class="login-From"
           >
-            登录
-          </el-button>
-        </el-form-item>
-      </el-form>
+            <el-form-item label="用户名" prop="username">
+              <el-input v-model="loginParams.username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" prop="password">
+              <el-input show-password v-model="loginParams.password"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button
+                type="primary"
+                @click="submitLoginForm('loginForm')"
+                class="btns"
+              >
+                登录
+              </el-button>
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
+
+        <!-- 注册页面 -->
+        <el-tab-pane label="注册" name="second" class="page">
+          <el-form
+            :label-position="labelPosition"
+            label-width="80px"
+            :model="registerParams"
+            :rules="registerRules"
+            ref="registerForm"
+            class="register-From"
+          >
+            <el-form-item label="用户名" prop="username">
+              <el-input v-model="registerParams.username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" prop="password">
+              <el-input
+                show-password
+                v-model="registerParams.password"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="确认密码" prop="confirmPassword">
+              <el-input
+                show-password
+                v-model="registerParams.confirmPassword"
+              ></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button
+                type="primary"
+                @click="submitregisterForm('registerForm')"
+                class="btns"
+              >
+                注册
+              </el-button>
+            </el-form-item>
+          </el-form>
+        </el-tab-pane>
+      </el-tabs>
     </div>
   </div>
 </template>
@@ -70,6 +113,45 @@ const submitLoginForm = (formName) => {
 }
 
 // 注册业务逻辑
+const activeName = ref('first')
+const registerForm = ref(null)
+const registerParams = reactive({
+  username: '',
+  password: '',
+  confirmPassword: ''
+})
+const registerRules = reactive({
+  username: [
+    { required: true, message: '请输入要注册的用户名', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    {
+      min: 6,
+      max: 18,
+      message: '长度在 6 到 18 个字符',
+      trigger: 'blur'
+    }
+  ],
+  confirmPassword: [
+    { required: true, message: '请再次输入密码', trigger: 'blur' },
+    {
+      min: 6,
+      max: 18,
+      message: '长度在 6 到 18 个字符',
+      trigger: 'blur'
+    }
+  ]
+})
+const submitregisterForm = (formName) => {
+  ctx.$refs[formName].validate((valid) => {
+    if (valid) {
+      console.log(1)
+    } else {
+      ElMessage.error('账号或密码不符合参数要求')
+    }
+  })
+}
 </script>
 
 <style scoped lang="less">
@@ -79,20 +161,26 @@ const submitLoginForm = (formName) => {
   display: flex;
   justify-content: center;
   align-items: center;
+
   .login-wrapper {
-    width: 300px;
-    height: 200px;
-    border-radius: 10px;
-    padding: 20px;
-    background-color: #fff;
-    margin-bottom: 200px;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
-  }
-  .login_From {
-    margin: 13px 10px 0px -19px;
-  }
-  .btns {
-    margin: 13px 0 0 35px;
+    // width: 360px;
+    // height: 302px;
+    // border-radius: 10px;
+    // padding: 20px 40px;
+    // background-color: #fff;
+    // margin-bottom: 200px;
+    // box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+    .page {
+      padding: 22px 18px;
+      height: 239px;
+    }
+    .login-From,
+    .register-From {
+      margin: 13px 10px 0px -11px;
+    }
+    .btns {
+      margin: 6px 36px auto;
+    }
   }
 }
 </style>
