ab878722bc3ab418f75a6816b7fc1dbcba717a66
SEVENTEEN <739829753@qq.com>
2021-09-22 21:55:16 +0800

    feat: 预览功能

--- src/pages/vote/components/vote-header.vue
+++ src/pages/vote/components/vote-header.vue
@@ -66,24 +66,21 @@
     </div>
   </div>
 
-  <el-drawer
-    v-model="drawer"
-    :direction="direction"
-    size="100%"
-    :with-header="false"
-  >
+  <el-drawer v-model="drawer" direction="ttb" size="100%" :with-header="false">
     <!-- 预览头部 -->
     <div class="drawer-container">
       <div class="drawer-header">
-        <div class="drawer-left">
+        <div class="drawer-left" @click="drawer = false">
           <i class="drawer-icon"></i>
           <span class="drawer-span">退出预览</span>
         </div>
         <div class="drawer-center">
           <span class="drawer-tip">提示：当前为预览页面，答案不被记录。</span>
         </div>
-        <div class="drawer-right">
-          <el-button type="primary">发布并分享</el-button>
+        <div v-if="curVoteDetail.status !== 1" class="drawer-right">
+          <el-button @click="handlePublish" type="primary">
+            发布并分享
+          </el-button>
         </div>
       </div>
       <div class="drawer-main">
@@ -96,17 +93,31 @@
           </div>
           <div class="main-blueWire"></div>
           <div class="question-box">
-            <div class="question-id">{{ voteTitle }}</div>
-          </div>
-          <div class="question-choose">
-            <div class="radio-container">
-              <div class="radio-select">
-                <el-radio :label="0">{{ voteSelect }}</el-radio>
-                <el-radio :label="1">
-                  {{ voteValue.name }}
-                </el-radio>
+            <h4 class="tip">
+              {{ tip }}
+              <span class="dot">*</span>
+            </h4>
+            <el-radio-group
+              v-if="curVoteDetail.isMultiple === 0"
+              v-model="choose"
+            >
+              <div v-for="item in curVoteDetail.optionList" :key="item.id">
+                <el-radio :label="item.id">{{ item.name }}</el-radio>
               </div>
-            </div>
+            </el-radio-group>
+            <el-checkbox-group v-else v-model="checkList">
+              <div v-for="item in curVoteDetail.optionList" :key="item.id">
+                <el-checkbox :label="item.id">{{ item.name }}</el-checkbox>
+              </div>
+            </el-checkbox-group>
+            <el-button
+              @click="fakeSubmit"
+              style="width: 100px; margin-top: 20px"
+              type="primary"
+              size="medium"
+            >
+              提交
+            </el-button>
           </div>
         </div>
       </div>
@@ -114,18 +125,22 @@
   </el-drawer>
 </template>
 <script setup>
-import { computed, ref, watch, reactive } from 'vue'
+import { computed, ref, watch, onMounted } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import { useStore } from 'vuex'
 import { RefreshRight, CircleCheck } from '@element-plus/icons'
 import { useVote } from '@/hooks/vote'
-import { ElMessageBox } from 'element-plus'
+import { ElMessageBox, ElLoading, ElNotification } from 'element-plus'
+
+onMounted(() => {
+  console.log(curVoteDetail.value)
+})
 
 const router = useRouter()
 const route = useRoute()
 const store = useStore()
 const isFirst = ref(true)
-const { curVoteDetail } = useVote()
+const { curVoteDetail, updateVote } = useVote()
 
 const loading = computed(() => store.state.vote.loading)
 const curVoteId = computed(() => route.params.id)
@@ -161,26 +176,57 @@ const toEdit = () => {
 
 const curQuestion = computed(() => store.state.vote.currentVoteDetail.name)
 
+// 抽屉预览逻辑
 const drawer = ref(false)
-const direction = ref('ttb')
-// 预览投票
 const voteTitle = computed(() => store.state.vote.currentVoteDetail.name)
-
-const voteValue = reactive({
-  id: '',
-  name: ''
-})
-
-const voteSelect = computed(() =>
-  store.state.vote.currentVoteDetail.optionList.forEach((key, index) => {
-    console.log(key.name)
-    console.log(index)
-    for (let index = 0; index <= key.name.length; index++) {
-      voteValue.name = key.name
-      console.log(voteValue.name)
-    }
-  })
+const tip = computed(() =>
+  curVoteDetail.value.isMultiple === 0
+    ? '请选择一个选项'
+    : '请选择一个或者多个选项'
 )
+const choose = ref('')
+const checkList = ref([])
+const fakeSubmit = () => {
+  ElMessageBox.confirm('当前提交内容不会做为记录哟~', '友情提示', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消'
+  })
+    .then(() => {})
+    .catch(() => {})
+}
+const handlePublish = () => {
+  ElMessageBox.confirm(
+    `你要发起投票《${curVoteDetail.value.name}》吗？`,
+    '发布确认',
+    {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消'
+    }
+  )
+    .then(async () => {
+      const elLoading = ElLoading.service({
+        fullscreen: true,
+        lock: true,
+        text: '发布中',
+        spinner: 'el-icon-loading'
+      })
+      await updateVote(route.params.id, {
+        ...curVoteDetail.value,
+        status: 1
+      })
+      setTimeout(() => {
+        elLoading.close()
+        ElNotification({
+          title: '成功',
+          message: '投票项目已成功发布',
+          type: 'success'
+        })
+      }, 500)
+
+      setTimeout(() => location.reload(), 800)
+    })
+    .catch(() => {})
+}
 </script>
 
 <style scoped lang="less">
@@ -263,7 +309,6 @@ const voteSelect = computed(() =>
         margin-right: 11px;
         background: url(https://s1.wenjuan.com/static/images/icon_edit_tools.png)
           no-repeat;
-        // background: url(../../../assets/images/vote_header_exit.svg)
         background-position: -360px -168px;
         background-size: 520px auto;
       }
@@ -293,24 +338,27 @@ const voteSelect = computed(() =>
   }
 
   .drawer-main {
-    padding: 145px 557px 145px;
+    padding-top: 100px;
     width: 100%;
     height: 100vh;
     background-color: #f5f5f5;
     .main-box {
-      // height: 675px;
+      margin: auto;
+      width: 814px;
       background-color: #ffffff;
       clear: both;
       overflow: hidden;
       padding: 10px 40px 66px;
     }
     .main-title {
-      font-size: 18px;
+      text-align: center;
+      color: #4c4c4c;
+      font-size: 24px;
       margin-top: 38px;
     }
     .main-welcome {
       margin-top: 26px;
-      font-size: 15px;
+      color: #484848;
     }
     .main-blueWire {
       background-color: #53a4f4 !important;
@@ -318,21 +366,21 @@ const voteSelect = computed(() =>
       width: 100%;
       height: 3px;
       margin-top: 26px;
+      margin-bottom: 20px;
     }
     .question-box {
-      width: 734px;
-      margin-top: 30px;
+      display: flex;
+      flex-direction: column;
       padding-left: 15px;
-      display: -webkit-inline-box;
-    }
-    .question-choose {
-      .radio-container {
-        .radio-select {
-          display: inline-block;
-          display: inline;
-          zoom: 1;
-          vertical-align: top;
-          line-height: 17px;
+      .tip {
+        margin: 0;
+        color: #666;
+        font-size: 16px;
+        font-weight: 400;
+        margin-bottom: 18px;
+        .dot {
+          color: #f56c6c;
+          font-size: 12px;
         }
       }
     }
